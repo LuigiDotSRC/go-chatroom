c5c25e1bc6cd8912e42df179d7ac599705172bf2
LuigiDotSRC <luigi.terashima@gmail.com>
2025-01-12 13:25:53 -0500

    implement standard echo server

--- server/server.go
+++ server/server.go
@@ -11,5 +11,23 @@ func StartServer() {
 		log.Fatalf("Could not initialize tcp socket: %v", err)
 	}
 
-	listener.Accept()
+	conn, _ := listener.Accept()
+	log.Printf("Server running on: %s", listener.Addr().String())
+	handler(conn)
+}
+
+func handler(conn net.Conn) {
+	defer conn.Close()
+	var out []byte = []byte("PONG")
+	for {
+		in := make([]byte, 1024)
+		n, err := conn.Read(in)
+		if err != nil {
+			log.Printf("Could not read input: %v", err)
+			return
+		}
+		if n > 0 {
+			conn.Write(out)
+		}
+	}
 }
