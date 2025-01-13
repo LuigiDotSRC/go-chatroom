673635be13497caac3418f007b7fcfacb528634c
LuigiDotSRC <luigi.terashima@gmail.com>
2025-01-12 20:23:48 -0500

    implement multithreading and message broadcast

--- server/server.go
+++ server/server.go
@@ -5,20 +5,33 @@ import (
 	"net"
 )
 
+var (
+	connections = make([]net.Conn, 0)
+)
+
 func StartServer() {
 	listener, err := net.Listen("tcp4", "127.0.0.1:5000")
 	if err != nil {
 		log.Fatalf("Could not initialize tcp socket: %v", err)
 	}
+	defer listener.Close()
 
-	conn, _ := listener.Accept()
 	log.Printf("Server running on: %s", listener.Addr().String())
-	handler(conn)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Could not accept connection: %v", err)
+			continue
+		}
+		connections = append(connections, conn)
+		go handler(conn)
+	}
 }
 
 func handler(conn net.Conn) {
 	defer conn.Close()
 	var out []byte = []byte("PONG")
+
 	for {
 		in := make([]byte, 1024)
 		n, err := conn.Read(in)
@@ -28,6 +41,13 @@ func handler(conn net.Conn) {
 		}
 		if n > 0 {
 			conn.Write(out)
+			broadcast(in)
 		}
+	}
+}
+
+func broadcast(msg []byte) {
+	for _, c := range connections {
+		c.Write(msg)
 	}
 }
